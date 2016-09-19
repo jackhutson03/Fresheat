7ab922567eefcb19f3170158d2960c80a9a77581
Jack Hutson <jack.hutson03@gmail.com>
2016-09-19 19:32:20 +0530

    Add files via upload

--- protected/extension/GoogleApis/lib/google-api-php-client/src/cache/apiMemcacheCache.php
+++ protected/extension/GoogleApis/lib/google-api-php-client/src/cache/apiMemcacheCache.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2010 Google Inc.
+ * Copyright 2008 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,40 +16,51 @@
  */
 
 /**
- * A persistent storage class based on the APC cache, which is not
- * really very persistent, as soon as you restart your web server
+ * A persistent storage class based on the memcache, which is not
+ * really very persistent, as soon as you restart your memcache daemon
  * the storage will be wiped, however for debugging and/or speed
  * it can be useful, kinda, and cache is a lot cheaper then storage.
  *
  * @author Chris Chabot <chabotc@google.com>
  */
-class apiApcCache extends apiCache {
+class apiMemcacheCache extends apiCache {
+  private $connection = false;
 
   public function __construct() {
-    if (! function_exists('apc_add')) {
-      throw new apiCacheException("Apc functions not available");
+    global $apiConfig;
+    if (! function_exists('memcache_connect')) {
+      throw new apiCacheException("Memcache functions not available");
+    }
+    $this->host = $apiConfig['ioMemCacheCache_host'];
+    $this->port = $apiConfig['ioMemCacheCache_port'];
+    if (empty($this->host) || empty($this->port)) {
+      throw new apiCacheException("You need to supply a valid memcache host and port");
     }
   }
 
   private function isLocked($key) {
-    if ((@apc_fetch($key . '.lock')) === false) {
+    $this->check();
+    if ((@memcache_get($this->connection, $key . '.lock')) === false) {
       return false;
     }
     return true;
   }
 
   private function createLock($key) {
+    $this->check();
     // the interesting thing is that this could fail if the lock was created in the meantime..
     // but we'll ignore that out of convenience
-    @apc_add($key . '.lock', '', 5);
+    @memcache_add($this->connection, $key . '.lock', '', 0, 5);
   }
 
   private function removeLock($key) {
+    $this->check();
     // suppress all warnings, if some other process removed it that's ok too
-    @apc_delete($key . '.lock');
+    @memcache_delete($this->connection, $key . '.lock');
   }
 
   private function waitForLock($key) {
+    $this->check();
     // 20 x 250 = 5 seconds
     $tries = 20;
     $cnt = 0;
@@ -64,27 +75,44 @@ class apiApcCache extends apiCache {
     }
   }
 
-   /**
+  // I prefer lazy initialization since the cache isn't used every request
+  // so this potentially saves a lot of overhead
+  private function connect() {
+    if (! $this->connection = @memcache_pconnect($this->host, $this->port)) {
+      throw new apiCacheException("Couldn't connect to memcache server");
+    }
+  }
+
+  private function check() {
+    if (! $this->connection) {
+      $this->connect();
+    }
+  }
+
+  /**
    * @inheritDoc
    */
   public function get($key, $expiration = false) {
-
-    if (($ret = @apc_fetch($key)) === false) {
+    $this->check();
+    if (($ret = @memcache_get($this->connection, $key)) === false) {
       return false;
     }
-    if (!$expiration || (time() - $ret['time'] > $expiration)) {
+    if (! $expiration || (time() - $ret['time'] > $expiration)) {
       $this->delete($key);
       return false;
     }
-    return unserialize($ret['data']);
+    return $ret['data'];
   }
 
   /**
    * @inheritDoc
    */
   public function set($key, $value) {
-    if (@apc_store($key, array('time' => time(), 'data' => serialize($value))) == false) {
-      throw new apiCacheException("Couldn't store data");
+    $this->check();
+    // we store it with the cache_time default expiration so objects will at least get cleaned eventually.
+    if (@memcache_set($this->connection, $key, array('time' => time(),
+        'data' => $value), false) == false) {
+      throw new apiCacheException("Couldn't store data in cache");
     }
   }
 
@@ -92,6 +120,7 @@ class apiApcCache extends apiCache {
    * @inheritDoc
    */
   public function delete($key) {
-    @apc_delete($key);
+    $this->check();
+    @memcache_delete($this->connection, $key);
   }
 }
