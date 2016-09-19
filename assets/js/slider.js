47c25382ed490053d82826df02daf35759af6edb
Jack Hutson <jack.hutson03@gmail.com>
2016-09-19 12:23:23 +0530

    Add files via upload

---
+++ assets/js/slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	'use strict';
+   	  HeaderVideo.init({
+      container: $('.header-video'),
+      header: $('.header-video--media'),
+      videoTrigger: $("#video-trigger"),
+      autoPlayVideo: true
+    });  
+    
+ 
+
+});
+
+
+//~ <script>
+ //~ $(function () {
+		 //~ 'use strict';
+ 
+    //~ });
+//~ </script>
